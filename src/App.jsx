f60e146ff50f38b6cf1a3e0c65d9fa57000daf9b
Situatedcake <leonovdanil18@gmail.com>
2026-03-18 22:20:28 +0700

    restore project, added new design, added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { Routes, Route} from "react-router-dom";
-import { Layout } from "./components/Layout";
-import MainPage from "./pages/MainPage";
-import TestingPage from "./pages/TestingPage";
-import TraningStartPage from "./pages/TraningStartPage";
+import "./App.css";
 
 export default function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage/>} />
-          <Route path="Test" element={<TestingPage />} />
-          <Route path="StartTraning" element={<TraningStartPage />}/>
-          
-        </Route>
-      </Routes>
-    </>
-  );
+  return <></>;
 }
